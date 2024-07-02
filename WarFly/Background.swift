13b20686a695f7c471cd4800ea7de256a72ea7bb
soaringman <sargonmeister@gmail.com>
2024-07-02 15:40:06 +0300

    Lesson 4
    Add background

---
+++ WarFly/Background.swift
@@ -0,0 +1,20 @@
+//
+//  Background.swift
+//  WarFly
+//
+//  Created by Алексей Гуляев on 02.07.2024.
+//
+
+import SpriteKit
+
+class Background: SKSpriteNode {
+	static func populateBackground(at point: CGPoint) -> Background {
+
+		let background = Background(imageNamed: "background")
+		background.position = point
+		background.zPosition = 0
+
+		return background
+	}
+
+}
