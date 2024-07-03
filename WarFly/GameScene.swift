5ccea9d7e5465e9410a162f68b44e2cfc1c4f59e
soaringman <sargonmeister@gmail.com>
2024-07-03 18:39:11 +0300

    lesson5
    Adding 5 islands random

--- WarFly/GameScene.swift
+++ WarFly/GameScene.swift
@@ -12,9 +12,23 @@ class GameScene: SKScene {
 
     override func didMove(to view: SKView) {
 		
+		//конфигурирование подложки
 		let screenCenterPoint = CGPoint(x: self.size.width / 2, y: self.size.height / 2)
 		let background = Background.populateBackground(at: screenCenterPoint)
 		background.size = self.size
 		self.addChild(background)
+
+		//вычисляем расположение 5-ти островов (создаются рандомные коорднаты островов в пределах экрана)
+		//Возможно дописать проверку на то, что бы острова не пересекались
+		let screen = UIScreen.main.bounds
+		for _ in 1...5 {
+			let randomX: CGFloat = CGFloat(GKRandomSource.sharedRandom().nextInt(upperBound:
+																					Int(screen.size.width)))
+			let randomY: CGFloat = CGFloat(GKRandomSource.sharedRandom().nextInt(upperBound:
+																					Int(screen.size.height)))
+			let island = Island.populateIsland(at: CGPoint(x: randomX, y: randomY))
+			
+			self.addChild(island)
+		}
     }
 }
---
+++ WarFly/Island.swift
@@ -0,0 +1,48 @@
+//
+//  Island.swift
+//  WarFly
+//
+//  Created by Алексей Гуляев on 03.07.2024.
+//
+
+import SpriteKit
+import GameplayKit
+
+class Island: SKSpriteNode {
+
+	static func populateIsland(at point: CGPoint) -> Island {
+		let islandImageName = configureIslandName()
+		let island = Island(imageNamed: islandImageName)
+
+		//конфигурируем остров
+		island.setScale(randomScaleFactor)
+		island.position = point
+		island.zPosition = 1
+		island.run(rotateForRandomAngle())
+
+		return island
+	}
+	//Метод - рандомное измение островов (имени выводимого острова)
+	static func configureIslandName() -> String {
+		let distribution = GKRandomDistribution(lowestValue: 1, highestValue: 4)
+		let randomNumber = distribution.nextInt()
+		let imageName = "is" + "\(randomNumber)"
+
+		return imageName
+	}
+	// Метод - Рандомное изменение масштаба острова
+	static var randomScaleFactor: CGFloat {
+		let distribution = GKRandomDistribution(lowestValue: 1, highestValue: 10)
+		let randomNumber = CGFloat(distribution.nextInt()) / 10
+
+		return randomNumber
+	}
+
+	//Метод - рандомное изменение угла поворота острова (с преобразованием градусов в радианы)
+	static func rotateForRandomAngle() -> SKAction {
+		let distribution = GKRandomDistribution(lowestValue: 0, highestValue: 360)
+		let randomNumber = CGFloat(distribution.nextInt())
+
+		return SKAction.rotate(toAngle: randomNumber * CGFloat(Double.pi / 180), duration: 0)
+	}
+}
