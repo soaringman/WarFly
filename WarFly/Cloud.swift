b16ef59301bcdc97bc356b92ca437e1791687b4b
soaringman <sargonmeister@gmail.com>
2024-07-07 18:41:42 +0300

    Lesson7
    Добавление движения для островов и облаков (тестирование на физ устройстве)

--- WarFly/Cloud.swift
+++ WarFly/Cloud.swift
@@ -21,6 +21,7 @@ final class Cloud: SKSpriteNode, IGameBackgroundSpritable {
 		cloud.setScale(randomScaleFactor)
 		cloud.position = point
 		cloud.zPosition = 10
+		cloud.run(move(from: point))
 
 		return cloud
 	}
@@ -38,5 +39,15 @@ final class Cloud: SKSpriteNode, IGameBackgroundSpritable {
 		let randomNumber = CGFloat(distribution.nextInt()) / 10
 
 		return randomNumber
+	}
+
+	//Метод - движение облаков
+	fileprivate static func move(from point: CGPoint) -> SKAction {
+		let movePoint = CGPoint(x: point.x, y: -200)
+		let moveDistance = point.y + 200
+		let movementSpeed: CGFloat = 15.0
+		let duration = moveDistance / movementSpeed
+		let rezult = SKAction.move(to: movePoint, duration: TimeInterval(duration))
+		return rezult
 	}
 }
--- WarFly/Island.swift
+++ WarFly/Island.swift
@@ -19,11 +19,12 @@ final class Island: SKSpriteNode, IGameBackgroundSpritable {
 		island.position = point
 		island.zPosition = 1
 		island.run(rotateForRandomAngle())
+		island.run(move(from: point))
 
 		return island
 	}
 	//Метод - рандомное измение островов (имени выводимого острова)
-	static func configureName() -> String {
+	fileprivate static func configureName() -> String {
 		let distribution = GKRandomDistribution(lowestValue: 1, highestValue: 4)
 		let randomNumber = distribution.nextInt()
 		let imageName = "is" + "\(randomNumber)"
@@ -31,7 +32,7 @@ final class Island: SKSpriteNode, IGameBackgroundSpritable {
 		return imageName
 	}
 	// Метод - Рандомное изменение масштаба острова
-	static var randomScaleFactor: CGFloat {
+	fileprivate static var randomScaleFactor: CGFloat {
 		let distribution = GKRandomDistribution(lowestValue: 1, highestValue: 10)
 		let randomNumber = CGFloat(distribution.nextInt()) / 10
 
@@ -39,10 +40,19 @@ final class Island: SKSpriteNode, IGameBackgroundSpritable {
 	}
 
 	//Метод - рандомное изменение угла поворота острова (с преобразованием градусов в радианы)
-	static func rotateForRandomAngle() -> SKAction {
+	fileprivate static func rotateForRandomAngle() -> SKAction {
 		let distribution = GKRandomDistribution(lowestValue: 0, highestValue: 360)
 		let randomNumber = CGFloat(distribution.nextInt())
 
 		return SKAction.rotate(toAngle: randomNumber * CGFloat(Double.pi / 180), duration: 0)
+	}
+	// Метод задающий движение островов
+	fileprivate static func move(from point: CGPoint) -> SKAction {
+		let movePoint = CGPoint(x: point.x, y: -200)
+		let moveDistance = point.y + 200
+		let movementSpeed: CGFloat = 10.0
+		let duration = moveDistance / movementSpeed
+		let rezult = SKAction.move(to: movePoint, duration: TimeInterval(duration))
+		return rezult
 	}
 }
