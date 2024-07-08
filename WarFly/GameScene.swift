b6a65281bf44765246b2cdf3df04add293c30c0d
soaringman <sargonmeister@gmail.com>
2024-07-08 16:54:15 +0300

    lesson8 part2
    добавление метода для акселерометра и определение логики движ самолета по х

--- WarFly/GameScene.swift
+++ WarFly/GameScene.swift
@@ -7,13 +7,20 @@
 
 import SpriteKit
 import GameplayKit
+import CoreMotion
 
 class GameScene: SKScene {
 
+	//Создаем менеджера - для считывания показаний акселерометра
+	let motionManager = CMMotionManager()
+	var xAcceleration: CGFloat = 0
+
+	//создаем наш самолет (если его нет то приложение должно упасть)
+	var player: SKSpriteNode!
+
+
+
     override func didMove(to view: SKView) {
-		
-		//создаем наш самолет (если его нет то приложение должно упасть)
-		var player: SKSpriteNode!
 
 		//конфигурирование подложки
 		let screenCenterPoint = CGPoint(x: self.size.width / 2, y: self.size.height / 2)
@@ -39,5 +46,30 @@ class GameScene: SKScene {
 
 		player = PlayerPlane.populate(at: CGPoint(x: screen.size.width / 2, y: 100))
 		self.addChild(player)
+
+		motionManager.accelerometerUpdateInterval = 0.2
+		motionManager.startAccelerometerUpdates(to: OperationQueue.current!) { (data, error) in
+
+			if let data = data {
+				let acceleration = data.acceleration
+				self .xAcceleration = CGFloat(acceleration.x) * 0.7 + self.xAcceleration * 0.3
+			}
+		}
     }
+
+	//Добавление метода в котором мы будем взаимедействовать с акселерометром нашего (физического устройства)
+	//так как у симулятора датчика акселерометра нет
+	override func didSimulatePhysics() {
+		super.didSimulatePhysics()
+
+		//Определяем логику поведения самолета по иси х
+		player.position.x += xAcceleration * 50
+
+		//Определение дествий если ушло влево или в право больше чем на 70 поинтов
+		if player.position.x < -70 {
+			player.position.x = self.size.width + 70
+		} else if player.position.x > self.size.width + 70 {
+			player.position.x = -70
+		}
+	}
 }
