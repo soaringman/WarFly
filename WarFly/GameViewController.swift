6ffafaf20e95f75b98abd2b328617ce52c1d1535
soaringman <sargonmeister@gmail.com>
2024-08-01 19:26:26 +0300

    lesson24
    adding start menu

--- WarFly/GameViewController.swift
+++ WarFly/GameViewController.swift
@@ -15,7 +15,7 @@ class GameViewController: UIViewController {
         super.viewDidLoad()
         
         if let view = self.view as! SKView? {
-			let scene = GameScene(size: self.view.bounds.size)
+			let scene = MenuScene(size: self.view.bounds.size)
                 // Set the scale mode to scale to fit the window
                 scene.scaleMode = .aspectFill
                 
---
+++ WarFly/MenuScene.swift
@@ -0,0 +1,39 @@
+//
+//  MenuScene.swift
+//  WarFly
+//
+//  Created by Алексей Гуляев on 01.08.2024.
+//
+
+import SpriteKit
+
+class MenuScene: SKScene {
+	override func didMove(to view: SKView) {
+		self.backgroundColor = SKColor(red: 0.15, green: 0.15, blue: 0.3, alpha: 1.0)
+		let texture = SKTexture(imageNamed: "play")
+		let button = SKSpriteNode(texture: texture)
+		button.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
+		button.name = "runButton"
+		self.addChild(button)
+	}
+	//ловим действие
+	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+		//ловим первое касание на текущем экране (место на которое мы нажали)
+		let location = touches.first!.location(in: self)
+		//метод addPoint позволяет получить обьект под той областью на которую мы нажали
+		//(предположительно наша кнопка)
+		let node = self.atPoint(location)
+		//теперь проверяем что под этой областью надодиться нода "runButton"
+		if node.name == "runButton" {
+			//то осуществить переход на другую сцену crossFade (один из вариантов перехода)
+			let transition = SKTransition.crossFade(withDuration: 1.0)
+			//теперь создадим ту цену на которую хотим перейти с размерами нашей текущей сцены
+			let gameScene = GameScene(size: self.size)
+			//масштаб сцены
+			gameScene.scaleMode = .aspectFill
+			//осуществим сам переход с тем видом (transition) который мы ранее выбрали
+			self.scene!.view?.presentScene(gameScene, transition: transition)
+		}
+	}
+
+}
