acae538abfad206b7cc239cfa98b43c045681a4b
soaringman <sargonmeister@gmail.com>
2024-08-08 22:15:42 +0300

    lesson29
    little refactor + adding user interface

--- WarFly/GameScene.swift
+++ WarFly/GameScene.swift
@@ -17,6 +17,13 @@ class GameScene: SKScene {
 	//создаем наш самолет (если его нет то приложение должно упасть)
 	var player: PlayerPlane!
 
+	//создаем элементы для пользовательского интерфейса
+	let scoreBackground = SKSpriteNode(imageNamed: "scores")
+	let scoreLabel = SKLabelNode(text: "1000")
+	let menuButton = SKSpriteNode(imageNamed: "menu")
+	let life1 = SKSpriteNode(imageNamed: "life")
+	let life2 = SKSpriteNode(imageNamed: "life")
+	let life3 = SKSpriteNode(imageNamed: "life")
 
 
     override func didMove(to view: SKView) {
@@ -31,18 +38,76 @@ class GameScene: SKScene {
 		configureStartScene()
 		spawnClouds()
 		spawnIsland()
-
-		//поправим баг -  в начале игры у нас вместо текстуры самолета появляется белый квадрат
-		//создаем переменную в которой говорим что мы хотим отсрочить запуск на 1 наносекунду
-		let deadline = DispatchTime.now() + .nanoseconds(1)
-		//отсрачиваем запуск нашего метода performFly
-		DispatchQueue.main.asyncAfter(deadline: deadline) { [unowned self] in
-			self.player.performFly()
-		}
-
+		self.player.performFly()
 		spawnPowerUp()
 		spawnEnemies()
+		configureUI()
     }
+
+	fileprivate func configureUI() {
+		//я корректировал позицию что бы не залезать на "остров"
+		//можно еще было поиграться с масштабом
+
+		//зададим нашу позицию относительно измененного далее анкорПоинта
+		scoreBackground.position = CGPoint(
+			x: scoreBackground.size.width + 10,
+			y: self.size.height - scoreBackground.size.height / 2 - 40
+		)
+
+		//сместим наш анкорПоинт
+		scoreBackground.anchorPoint = CGPoint(x: 1.0, y: 0.5)
+		scoreBackground.zPosition = 99
+		addChild(scoreBackground)
+
+		//наш ярлык с очками всегда долбен быть внутри нашего scoreBackground
+		//для этого выравниваем наш ярлык по горизонтали и вертикали
+		scoreLabel.horizontalAlignmentMode = .right
+		scoreLabel.verticalAlignmentMode = .center
+
+		//слегка подравниваем внутри (подобран опытным путем)
+		scoreLabel.position = CGPoint(x: -10, y: 3)
+
+		//зададим позицию по оси z
+		scoreLabel.zPosition = 100
+
+		//задаем фонт, предварительно его подсмотрев тут: iosfonts.com
+		scoreLabel.fontName = "AmericanTypewriter-Bold"
+
+		//зададим размер (подобран опытным путем)
+		scoreLabel.fontSize = 30
+
+		//добавим на экран
+		scoreBackground.addChild(scoreLabel)
+
+		//теперь добавим нашу кнопку "меню"
+		menuButton.position = CGPoint(x: 20, y: 20)
+
+		//так жа сместим наш анкор поинт
+		//(обучно по умолчанию он находиться в центре фигуры)
+		menuButton.anchorPoint = CGPoint(x: 0.0, y: 0.0)
+
+		menuButton.zPosition = 100
+		addChild(menuButton)
+
+		//и теперь добавим "жизни" для самолета
+		let lifes = [life1, life2, life3]
+		
+		//метод enumerated позволяет возвращать сам элемент и его индекс
+		for (index, life) in lifes.enumerated() {
+
+			//мы шагаем от правой границы экрана по х влево на ширину звезды
+			//и прокручивая это в цикле получаем смещение на 3, потом на 2
+			//потом на 1 звезду, между звездами расстояние 3 поинта
+			life.position = CGPoint(
+				x: self.size.width - 10 - CGFloat(index + 1) * (life.size.width + 3),
+				y: 20
+			)
+
+			life.zPosition = 100
+			life.anchorPoint = CGPoint(x: 0.0, y: 0.0)
+			addChild(life)
+		}
+	}
 
 	fileprivate func spawnPowerUp() {
 
